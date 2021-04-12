e856d195ace789b40f09567bdd74dd6f8466c995
FanWu <309326243@qq.com>
2021-04-12 21:08:53 +0800

    commit pipeline for kafka producer->consumer->fit nb model

--- src/main/scala/com/teamone/spark/LDAtest.scala
+++
@@ -1,8 +0,0 @@
-package com.teamone.spark
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.sql.Row
-import org.apache.spark.mllib.linalg.Vector
-import org.apache.spark.mllib.clustering.{DistributedLDAModel, LDA}
-object LDAtest extends App {
-
-}
