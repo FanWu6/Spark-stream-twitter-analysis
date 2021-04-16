93a1d07b392b09ef456677566637ccddb44cfb7b
Dayu <jia.21big@gmail.com>
2021-04-16 16:34:17 +0800

    Write to elasticsearch
    It seems like it works well, NEED Kibana to confirm

--- src/main/scala/com/teamone/UI/WriteToElastic.scala
+++ src/main/scala/com/teamone/UI/WriteToElastic.scala
@@ -6,6 +6,10 @@ import java.util.Date
 import org.apache.spark.sql.functions._
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import org.elasticsearch.spark._
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
 
 import java.text.SimpleDateFormat
 
@@ -14,6 +18,19 @@ object WriteToElastic {
   def main(args: Array[String]): Unit = {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
+
+    /*val conf = new SparkConf().setAppName("WtE").setMaster("local[*]")
+    conf.set("es.index.auto.create", "true")
+
+    val sc = new SparkContext(conf)
+
+    val numbers = Map("one" -> 1, "two" -> 2, "three" -> 3)
+    val airports = Map("arrival" -> "Otopeni", "SFO" -> "San Fran")
+
+    sc.makeRDD(
+      Seq(numbers, airports)
+    ).saveToEs("spark/docs")*/
+
     val sc = SparkSession.builder().appName("WriteToElastic").master("local[*]").getOrCreate()
 
 
@@ -24,12 +41,11 @@ object WriteToElastic {
 
     // Write DF to elastic search
     lines.write
-      .format("org.elastic.spark.sql")
+      .format("org.elasticsearch.spark.sql")
       .option("es.port", 9200)
       .option("es.host", "localhost")
       .mode("append")
       .save("usedcarspark/doc")
-
   }
 
 }
