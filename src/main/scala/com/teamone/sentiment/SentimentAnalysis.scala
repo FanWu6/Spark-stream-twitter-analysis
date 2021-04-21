38f97bf6b1aa5150ac5cc9248182fb19379b470f
DJia <jia.21big@gmail.com>
2021-04-21 13:08:37 +0800

    Annotation

--- src/main/scala/com/teamone/sentiment/SentimentAnalysis.scala
+++ src/main/scala/com/teamone/sentiment/SentimentAnalysis.scala
@@ -14,17 +14,36 @@ import scala.collection.convert.wrapAll._
 object SentimentAnalysis {
 
   val props = new Properties()
+
+  // Clean data
   props.setProperty("annotators", "tokenize, ssplit, pos, lemma, parse, sentiment")
   val pipeline: StanfordCoreNLP = new StanfordCoreNLP(props)
 
+  /**
+   *
+   * @param input List of tweets
+   * @return List of tweets & sentiments
+   */
   def twAndSentiments(input: List[String]): List[(String, Sentiment)] =  input map twAndSentiment
 
+  /**
+   *
+   * @param input A tweet
+   * @return Tweet itself and its sentiment analysis
+   */
   def twAndSentiment(input: String): (String, Sentiment) = (input, mainSentiment(input))
 
+  /**
+   *
+   * @param input List of tweets
+   * @return List of sentiments
+   */
   def mainSentiments(input: List[String]): List[Sentiment] = input map mainSentiment
 
   /**
-   * Extracts the main sentiment for a given input
+   *
+   * @param input A tweet
+   * @return The main sentiment for a given tweet
    */
   def mainSentiment(input: String): Sentiment = Option(input) match {
     case Some(text) if text.nonEmpty => extractSentiment(text)
@@ -32,19 +51,25 @@ object SentimentAnalysis {
   }
 
   /**
-   * Extracts a list of sentiments for a given input
+   * Another way to extract a list of sentiments
    */
   def sentiment(input: String): List[(String, Sentiment)] = Option(input) match {
     case Some(text) if text.nonEmpty => extractSentiments(text)
     case _ => throw new IllegalArgumentException("input can't be null or empty")
   }
 
+  /**
+   * Extracts sentiment for a given input
+   */
   private def extractSentiment(text: String): Sentiment = {
     val (_, sentiment) = extractSentiments(text)
       .maxBy { case (sentence, _) => sentence.length }
     sentiment
   }
 
+  /**
+   * Extracts list of texts and sentiments for a given input list
+   */
   private def extractSentiments(text: String): List[(String, Sentiment)] = {
     val annotation: Annotation = pipeline.process(text)
     val sentences = annotation.get(classOf[CoreAnnotations.SentencesAnnotation])
